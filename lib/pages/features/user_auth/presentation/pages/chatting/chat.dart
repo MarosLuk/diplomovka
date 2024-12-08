84e149b228aa7f9f274c7bf03afea36f6abd9b78
maros.lukac <maros.lukac@asseco-ce.com>
2024-12-08 20:23:43 +0100

    mention redirect

--- lib/pages/features/user_auth/presentation/pages/chatting/chat.dart
+++ lib/pages/features/user_auth/presentation/pages/chatting/chat.dart
@@ -6,6 +6,7 @@ import 'package:diplomovka/pages/features/app/providers/user_provider.dart';
 import 'package:diplomovka/assets/colorsStyles/text_and_color_styles.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:diplomovka/pages/features/app/global/toast.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatPage extends ConsumerStatefulWidget {
   final String problemId;
@@ -33,17 +34,88 @@ class _ChatPageState extends ConsumerState<ChatPage> {
   bool _showScrollToBottomButton = false;
   Timer? _fetchTimer;
 
+  bool _isMentioning = false;
+  List<Map<String, dynamic>> availableContainers = [];
+
   @override
   void initState() {
     super.initState();
 
     _startFetchingMessages();
+    _fetchContainers();
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _scrollToBottom();
     });
 
     _scrollController.addListener(_onScroll);
+  }
+
+  Future<void> _fetchContainers() async {
+    final firestore = FirebaseFirestore.instance;
+    final problemRef = firestore.collection('problems').doc(widget.problemId);
+    final snapshot = await problemRef.get();
+
+    if (snapshot.exists) {
+      final problemData = snapshot.data();
+      if (problemData != null && problemData.containsKey('containers')) {
+        final containersRaw = problemData['containers'] as List<dynamic>;
+        setState(() {
+          availableContainers = containersRaw.map((c) {
+            return {
+              'containerName': c['containerName'],
+              'containerId': c['containerId'],
+            };
+          }).toList();
+        });
+      }
+    }
+  }
+
+  void _onMessageChanged(String value) {
+    if (value.contains('@') && !_isMentioning) {
+      setState(() {
+        _isMentioning = true;
+      });
+    } else if (!value.contains('@') && _isMentioning) {
+      setState(() {
+        _isMentioning = false;
+      });
+    }
+  }
+
+  void _onContainerSelected(String containerName) {
+    final text = messageController.text;
+    final mentionIndex = text.lastIndexOf('@');
+    if (mentionIndex != -1) {
+      final newText = text.substring(0, mentionIndex + 1) + containerName + " ";
+      messageController.text = newText;
+      messageController.selection = TextSelection.fromPosition(
+        TextPosition(offset: newText.length),
+      );
+      setState(() {
+        _isMentioning = false;
+      });
+    }
+  }
+
+  void _onMentionClicked(String containerName) {
+    final selectedContainer = availableContainers.firstWhere(
+      (c) => c['containerName'] == containerName,
+      orElse: () => {},
+    );
+    if (selectedContainer != null) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ChatPage(
+            problemId: widget.problemId,
+            chatId: selectedContainer['containerId'],
+            containerName: selectedContainer['containerName'],
+          ),
+        ),
+      );
+    }
   }
 
   void _startFetchingMessages() {
@@ -218,23 +290,33 @@ class _ChatPageState extends ConsumerState<ChatPage> {
                                     color: Theme.of(context).primaryColor,
                                   ),
                                 ),
-                                ConstrainedBox(
-                                  constraints: BoxConstraints(
-                                    maxWidth:
-                                        MediaQuery.of(context).size.width * 0.6,
-                                  ),
-                                  child: Container(
-                                    padding: const EdgeInsets.all(12),
-                                    decoration: BoxDecoration(
-                                      color: isCurrentUser
-                                          ? Colors.blue[400]
-                                          : Colors.black45,
-                                      borderRadius: BorderRadius.circular(8),
+                                GestureDetector(
+                                  onTap: () {
+                                    if (message['message'].startsWith('@')) {
+                                      _onMentionClicked(message['message']
+                                          .substring(1)
+                                          .trim());
+                                    }
+                                  },
+                                  child: ConstrainedBox(
+                                    constraints: BoxConstraints(
+                                      maxWidth:
+                                          MediaQuery.of(context).size.width *
+                                              0.6,
                                     ),
-                                    child: Text(
-                                      message['message'],
-                                      style: AppStyles.titleSmall(
-                                        color: Theme.of(context).primaryColor,
+                                    child: Container(
+                                      padding: const EdgeInsets.all(12),
+                                      decoration: BoxDecoration(
+                                        color: isCurrentUser
+                                            ? Colors.blue[400]
+                                            : Colors.black45,
+                                        borderRadius: BorderRadius.circular(8),
+                                      ),
+                                      child: Text(
+                                        message['message'],
+                                        style: AppStyles.titleSmall(
+                                          color: Theme.of(context).primaryColor,
+                                        ),
                                       ),
                                     ),
                                   ),
@@ -245,6 +327,22 @@ class _ChatPageState extends ConsumerState<ChatPage> {
                         },
                       ),
                     ),
+                    if (_isMentioning)
+                      Container(
+                        height: 200,
+                        color: Colors.white,
+                        child: ListView.builder(
+                          itemCount: availableContainers.length,
+                          itemBuilder: (context, index) {
+                            final container = availableContainers[index];
+                            return ListTile(
+                              title: Text(container['containerName']),
+                              onTap: () => _onContainerSelected(
+                                  container['containerName']),
+                            );
+                          },
+                        ),
+                      ),
                     Padding(
                       padding: const EdgeInsets.only(left: 12.0),
                       child: Row(
@@ -253,6 +351,7 @@ class _ChatPageState extends ConsumerState<ChatPage> {
                           Expanded(
                             child: TextField(
                               controller: messageController,
+                              onChanged: _onMessageChanged,
                               style: TextStyle(
                                 color: Theme.of(context).primaryColor,
                               ),
